405711c0fca3c5cabed9a53c3668cd507b1b374a
VaRe <82159995+OgurecMatvey@users.noreply.github.com>
2025-06-02 19:58:40 +0300

    Add files via upload

---
+++ assets/maps/Background_02.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Background_02" tilewidth="32" tileheight="32" tilecount="943" columns="41">
+ <image source="tilesets/ruins/PNG/Background/Background_02.png" width="1334" height="750"/>
+</tileset>
